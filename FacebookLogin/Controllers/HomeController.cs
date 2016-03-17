88ce326a22157fd9705bb7049db8be574f0f95c1
Cameron Graham <camy117@gmail.com>
2016-03-17 13:47:45 +0000

    Added to github

---
+++ FacebookLogin/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Facebook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FacebookLogin.Controllers
+{
+    [RequireHttps][Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
